1a903d02e1135e06cde2b433d7b437dbf563c75c
Brittany <bgreensl@ualberta.ca>
2021-04-02 12:53:10 -0600

    finished assigmnt and comments

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
 //For an API like this, that has different amounts of info each time, how could you
 //make it more dynamic so you can account for some cocktails having 2 ingredients
-//and some having like 10 or something?
-// let ajax;
+//and some having more ingredients?
+
+//declared a function that is called when the button is clicked that grabs a new
+//cocktail and all of its recipe info from the API URL
 function nextCocktail() {
+  //set our AJAX variable
   let ajax = new XMLHttpRequest();
+  //this is the 'callback function'
   ajax.onreadystatechange = function () {
+    //4 is when the network request is done; 200 means there were no errors
+    //if all of these numbers come back a-ok, then........
     if (this.readyState == 4 && this.status == 200) {
-      console.log(this.responseText);
+      //JSON.parse turns the giant messy string returned from the API into a pretty lil
+      //object. we store this object in a variable for easy access down the road
       let responseTextObject = JSON.parse(this.responseText);
       //   console.log(responseTextObject);
-      document.getElementById("loading").style.display = `block`;
+      //go into the object and find the item with the name "drinks"; set it to a variable
       let cocktails = responseTextObject.drinks;
-      document.getElementById("loading").style.display = `none`;
-
+      //the 'drinks' item has a value that is an array, so in order to get any of the info out of
+      //that, we need to loop through it
       for (i = 0; i < cocktails.length; i++) {
+        //store whatever item that's returned from the array in a variable
         let cocktail = cocktails[i];
+        //check it out so we can get the info for the following grabs
         console.log(cocktail);
+        //inject the name, pic, glass type, ingredients, measurements, etc onto the page
         document.getElementById(
           "cocktailName"
         ).innerHTML = `<h1>${cocktail.strDrink}</h1>`;
@@ -24,11 +34,13 @@ function nextCocktail() {
         document.getElementById(
           "cocktailPic"
         ).innerHTML = `<img src = "${cocktailPic}">`;
-
         document.getElementById(
           "cocktailGlass"
         ).innerHTML = `<h2>${cocktail.strGlass}</h2>`;
-
+        //ok I know there HAS to be a better way to do the following involving classes
+        //that I'm going to try and figure out but my brain is tired of loops after
+        //the mess that is the object within the array within the object for this API
+        //so plz forgive me
         document.getElementById(
           "ingred1"
         ).innerHTML = `<p>${cocktail.strIngredient1}</p>`;
@@ -76,11 +88,8 @@ function nextCocktail() {
         document.getElementById(
           "msrmt6"
         ).innerHTML = `<p>${cocktail.strMeasure1}</p>`;
-
-        //uhm there HAS to be a better way to do this involving classes that I'm going to
-        //try and figure out but my brain is tired of loops after trying to figure out
-        //how to get into the object within the array within the object for this
-        //so plz forgive me
+        //some cocktails had less ingredients and data from the request comes back 'null' so I
+        //hid this if that was the case
         if (cocktail.strIngredient3 === null) {
           document.getElementById("ingred3").style.display = `none`;
           document.getElementById("msrmt3").style.display = `none`;
@@ -103,26 +112,36 @@ function nextCocktail() {
       }
     }
   };
+  //configures the request (says where it's going, that it's going to 'get' data, and says
+  //'true' to the request being asynchronous because sycnhronous = bad/scary)
   ajax.open(
     "GET",
     "https://www.thecocktaildb.com/api/json/v1/1/random.php",
     true
   );
 
+  //This function injects a 'loading' message on the page when the next cocktail revipe
+  //is loading. 'display: block' because it wasn't showing up without a display setting
+  //but I'm not sure why because it doesn't have a display setting otherwise and I
+  //thought it would just be injected 'onloadstart'
   ajax.onloadstart = function loading() {
     document.getElementById("loading").innerHTML = `<h2>Loading...</h2>`;
     document.getElementById("loading").style.display = `block`;
   };
-
-  ajax.send();
-  //I think this is how to set on error but I can't figure out how to make it work
+  //This is how I found to set an error message but it's not possible to see it unless there's
+  //an actual error at the network level not just at the app level right or?
   ajax.onerror = function () {
     document.getElementById(
       "loading"
     ).innerHTML = `<h2>ERROR 404: Drink water instead</h2>`;
   };
+  //the place I found for error and on-load message said to put those two functions before
+  //the send request - presumably because they'll be 'left out' of the request and ignored
+  //aka won't work
+
+  //this sends the AJAX request to the API to 'get' the info
+  ajax.send();
 }
 let cocktailBtn = document.getElementById("cocktailBtn");
 cocktailBtn.addEventListener("click", nextCocktail);
-//also something is showing up undefined in the console and I don't know what? because
-// everything appears to be working
+//called the giant 'newCocktail' function declared above to show a different cocktail on click
